e55eda4ad18bff797db72ca1f0c11b8fe387f17d
2783303761 <2783303761@qq.com>
2023-06-16 16:20:23 +0800

    feat:添加footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,15 +3,18 @@
   <div class="main-container">
     <slot></slot>
   </div>
+  <Footer></Footer>
 </template>
 <script setup>
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 </script>
 <style lang="less" scoped>
 .main-container {
-  max-width: 940px;
+  max-width: 1040px;
   margin: 0 auto;
+  padding: 0 50px 50px 50px;
 }
 </style>
 <style lang="less">

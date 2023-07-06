b940c80907c390bc782cc139b71a2eb092c26a73
2783303761 <2783303761@qq.com>
2023-07-06 11:18:30 +0800

    feat:详情页评论

--- pages/p/[id]/index.vue
+++ pages/p/[id]/index.vue
@@ -1,9 +1,11 @@
 <template>
   <ArticleDetail></ArticleDetail>
+  <Comment></Comment>
 </template>
 
 <script setup>
 import ArticleDetail from './components/ArticleDetail.vue';
+import Comment from './components/Comment.vue';
 </script>
 
 <style>

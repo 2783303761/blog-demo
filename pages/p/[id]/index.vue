1bb5d2dfbe10d51c0de4ccab53d7cf149630a401
2783303761 <2783303761@qq.com>
2023-07-06 11:30:02 +0800

    feat:详情页推荐

--- pages/p/[id]/index.vue
+++ pages/p/[id]/index.vue
@@ -1,11 +1,13 @@
 <template>
   <ArticleDetail></ArticleDetail>
   <Comment></Comment>
+  <Recommend></Recommend>
 </template>
 
 <script setup>
 import ArticleDetail from './components/ArticleDetail.vue';
 import Comment from './components/Comment.vue';
+import Recommend from './components/Recommend.vue';
 </script>
 
 <style>
